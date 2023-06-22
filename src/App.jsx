06961d2eb500a202c47c299e6b1c0a0331289f60
Austin Obnillas <austinobnillas@gmail.com>
2023-06-21 22:58:02 -0700

    Todo list assignment completed - single page version

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  //state 
+  const [listItem, setListItem] = useState("");
+  const [list, setList] = useState([]);
+
+  //submit function
+  const submitListItem = (event) => {
+    event.preventDefault();
+    setList([...list, todoItem])
+  };
+
+  // object for list items
+  const todoItem = {
+    text: listItem,
+    complete: false
+  };
+
+  //delete function
+  const handleDelete = (deleteIndex) => {
+    const updatedList = list.filter((listItem, i) => {
+      return i != deleteIndex;
+    });
+    setList(updatedList);
+  };
+
+  //completed checkbox
+  const handleComplete = (id) => {
+    const updatedList = list.map((listItem, i) => {
+      if (id === i) {
+        listItem.complete = !listItem.complete;
+      }
+      return listItem
+    });
+    setList(updatedList)
+  }
+
+  //JSX
+  return (
+    <div className="App">
+      <h1>TODO List</h1>
+            {/*FORM SECTION*/}
+            <form onSubmit={(event) => {submitListItem(event);}}>
+              <input 
+                type="text" 
+                onChange={ (event) => setListItem(event.target.value)}
+              />
+                <div>
+                    <button>Add</button>
+                </div>
+            </form>
+            {/*Display TODO LIST*/} 
+            {list.map((listItem, i) => {
+              //line through text styling once checked complete
+              const completed = [];
+              if (listItem.complete){
+                completed.push("completed")
+              }
+              //list
+              return <div key={i}>
+                <span className={completed}>{listItem.text}</span>
+                <input onChange = {(event) => {
+                  handleComplete(i);
+                }} 
+                checked={listItem.complete} 
+                type="checkbox"/>
+                <button onClick={(event) => { handleDelete(i); }}>Delete</button>
+              </div>
+            })}
+    </div>
+  );
+}
+
+export default App;
